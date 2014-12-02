a1eec542623d93a7b38483ce090eaed1696834f5
Adam Wohlberg <adam.wolhberg@gmail.com>
2014-12-02 09:53:25 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sukitfacebook::Application.config.secret_token = 'fd6376458391fa86af8b5ae2dd77ba2f75686b8a367f82560a4e144fdbe14196d981d4962ebdfc737a5078f88ea810f75bbd01b8a2773bc7b201f12882e60862'
